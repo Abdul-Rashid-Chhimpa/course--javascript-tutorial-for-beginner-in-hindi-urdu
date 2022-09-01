b4178ca54e42f405b14d960f1ff3ce08127a5a39
Abdul Rashid Chhimpa <110084078+Rashid789@users.noreply.github.com>
2022-09-02 02:31:59 +0530

    Add files via upload

---
+++ js/switch case statemant.js
@@ -0,0 +1,20 @@
+<script>
+        var day=prompt("enter the number: ")-0;
+       switch (day)
+        {
+       case 1:console.log(" Sunday");
+          break;
+       case 2:console.log(" Monday");
+          break;
+       case 3:console.log(" Thuesday");
+          break;
+       case 4:console.log(" Wednsday");
+          break;
+       case 5:console.log(" Thrusday");
+          break;
+       case 6:console.log(" Friday");
+          break;
+       case 7:console.log(" Saturday");
+          default:alert("invalid");
+     }
+  </script>
